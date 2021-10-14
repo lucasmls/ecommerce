5bc00102d7d64c2d14c3a0d346ea2952c1c34469
Lucas Mendes <lucaslsbh1@gmail.com>
2021-10-14 07:27:13 -0300

    Add shared package containing a way to create a gRPC server

---
+++ shared/go.mod
@@ -0,0 +1,19 @@
+module github.com/lucasmls/ecommerce/shared
+
+go 1.17
+
+require (
+	go.uber.org/zap v1.19.1
+	google.golang.org/grpc v1.41.0
+)
+
+require (
+	github.com/golang/protobuf v1.4.3 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
---
+++ shared/grpc/server.go
@@ -0,0 +1,63 @@
+package grpc
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+
+	"go.uber.org/zap"
+	gGRPC "google.golang.org/grpc"
+)
+
+// ServiceInput is the input (aka dependencies) to create a GRPC server.
+type ServerInput struct {
+	Port        int
+	Registrator func(server gGRPC.ServiceRegistrar)
+}
+
+// Server is the GRPC server itself.
+type Server struct {
+	in ServerInput
+
+	server *gGRPC.Server
+	logger *zap.Logger
+}
+
+// NewServer is the GRPC Server constructor.
+func NewServer(in ServerInput) (*Server, error) {
+	if in.Port <= 80 {
+		return nil, errors.New("the GRPC server port should be greater than or equal to 80")
+	}
+
+	if in.Registrator == nil {
+		return nil, errors.New("missing required dependency: Registrator")
+	}
+
+	grpcServer := gGRPC.NewServer()
+
+	return &Server{
+		in:     in,
+		server: grpcServer,
+	}, nil
+}
+
+func (s Server) Run(ctx context.Context) error {
+	s.in.Registrator(s.server)
+
+	address := fmt.Sprintf(":%d", s.in.Port)
+
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		s.logger.Error("Failed to start to listen TCP address", zap.Error(err))
+		return err
+	}
+
+	fmt.Println("gRPC server started in port:", s.in.Port)
+	if err := s.server.Serve(listener); err != nil {
+		s.logger.Error("Failed to serve gRPC server", zap.Error(err))
+		return err
+	}
+
+	return nil
+}
