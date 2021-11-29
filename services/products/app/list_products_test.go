3e1ca136eb01a021bd4c1d50869105645f5ea587
Lucas Mendes <lucaslsbh1@gmail.com>
2021-11-28 22:40:16 -0300

    Add unit test for ListProducts

---
+++ services/products/app/list_products_test.go
@@ -0,0 +1,52 @@
+package app
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/lucasmls/ecommerce/services/products/domain"
+	"github.com/lucasmls/ecommerce/services/products/mocks"
+	"go.uber.org/zap"
+)
+
+func TestListProducts(t *testing.T) {
+	tt := []struct {
+		name                string
+		expectedResult      error
+		productsRepositoryM func(context.Context, *gomock.Controller) domain.ProductsRepository
+	}{
+		{
+			name:           "Fails when repository.List returns a error",
+			expectedResult: errors.New("failed to list products from the datastore"),
+			productsRepositoryM: func(c context.Context, g *gomock.Controller) domain.ProductsRepository {
+				productsRepoM := mocks.NewMockProductsRepository(g)
+
+				productsRepoM.EXPECT().
+					List(c, domain.ListProductsFilter{}).
+					Return([]domain.Product{}, errors.New("failed to list products from the datastore"))
+
+				return productsRepoM
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.Background()
+			ctrl := gomock.NewController(t)
+			logger, _ := zap.NewDevelopment()
+
+			a := application{
+				logger:             logger,
+				productsRepository: tc.productsRepositoryM(ctx, ctrl),
+			}
+
+			_, err := a.ListProducts(ctx, domain.ListProductsFilter{})
+			if err.Error() != tc.expectedResult.Error() {
+				t.Errorf("application.ListProducts() = %v, want %v", err, tc.expectedResult)
+			}
+		})
+	}
+}
